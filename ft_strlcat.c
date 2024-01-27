f7c4fb75ac35d768669b55e347ffd84917b3b88c
edoyosa <edo.bellini97@gmail.com>
2024-01-27 01:14:37 +0100

    finished strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebellini <ebellini@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 22:34:31 by ebellini          #+#    #+#             */
+/*   Updated: 2024/01/27 01:06:09 by ebellini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dstlen;
+	size_t	i;
+	size_t	srclen;
+
+	i = 0;
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (!dst && !src)
+		return (0);
+	if (size == 0)
+		return (srclen);
+	while ((i + dstlen) < (size - 1) && src[i])
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	if (dstlen < size)
+		dst[dstlen + i] = '\0';
+	else
+		return (srclen + size);
+	return (dstlen + srclen);
+}
