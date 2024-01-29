89e6046b12ef72030695e3e8cda2f4dc0543d172
edoyosa <edo.bellini97@gmail.com>
2024-01-29 15:05:01 +0100

    modified calloc split and substr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,22 @@
 /*   By: ebellini <ebellini@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 00:10:11 by ebellini          #+#    #+#             */
-/*   Updated: 2024/01/28 15:49:35 by ebellini         ###   ########.fr       */
+/*   Updated: 2024/01/29 15:02:32 by ebellini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//DA RIVEDERE TEST 4 FRAU
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	int		length;
+	size_t	length;
 
-	if (size == SIZE_MAX || nmemb == SIZE_MAX)
+	if (size == 0 || nmemb == 0)
+		return (0);
+	if (SIZE_MAX / nmemb < size)
 		return (0);
 	length = nmemb * size;
-	if (length < 0)
-		return (0);
 	ptr = malloc(length);
 	if (!ptr)
 		return (0);
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ebellini <ebellini@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:30:41 by ebellini          #+#    #+#             */
-/*   Updated: 2024/01/28 19:21:28 by ebellini         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:40:01 by ebellini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	slength;
 
 	i = 0;
-	slength = ft_strlen(s);
 	if (!s)
 		return (0);
+	slength = ft_strlen(s);
 	if (start >= slength)
 		str = ft_strdup("");
-	else if (len < slength - start)
+	else if (len <= slength - start)
 		str = (char *)malloc(sizeof(char) * (len + 1));
 	else
-		str = (char *)malloc(sizeof(char) * (slength - start));
+		str = (char *)malloc(sizeof(char) * (slength - start + 1));
 	if (!str)
 		return (0);
-	while (s[i + start] && i < len && start < slength)
+	while (i < len && (i + start) < slength)
 	{
 		str[i] = s[i + start];
 		i++;
