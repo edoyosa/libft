a62d93036acdb401c0fbc388e52cb73911f8045b
edoyosa <edo.bellini97@gmail.com>
2024-01-29 18:52:39 +0100

    modified itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ebellini <ebellini@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 16:52:25 by ebellini          #+#    #+#             */
-/*   Updated: 2024/01/29 18:41:56 by ebellini         ###   ########.fr       */
+/*   Updated: 2024/01/29 18:49:13 by ebellini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,13 @@ char	*ft_createnum(char *str, long n, size_t len, int sign)
 {
 	unsigned char	c;
 
+	str[len + sign] = 0;
 	if (sign)
-	{
 		str[0] = '-';
-		str[len + 1] = 0;
-	}
-	else
-		str[len] = 0;
 	while (len-- > 0)
 	{
 		c = 48 + (n % 10);
-		str[len] = c;
+		str[len + sign] = c;
 		n /= 10;
 	}
 	return (str);
