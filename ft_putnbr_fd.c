70097f466c11c1a418c5cabff6021cdd39ddd968
edoyosa <edo.bellini97@gmail.com>
2024-01-30 14:32:06 +0100

    finished libft without bonus

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebellini <ebellini@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 13:33:35 by ebellini          #+#    #+#             */
+/*   Updated: 2024/01/30 14:24:37 by ebellini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	*str;
+
+	str = ft_itoa(n);
+	if (!str)
+		return ;
+	ft_putstr_fd(str, fd);
+	free (str);
+}
